4624d70cd9ee31e8d21c1f3d6553d3936e430ad5
Sunlee <303001274@qq.com>
2022-09-21 16:17:24 +0800

    feat: event use goroutine

--- main.go
+++ main.go
@@ -15,37 +15,42 @@ func main() {
 		log.Fatal(err)
 	}
 	startWatch(watcher)
+	log.Println("ok")
 	defer watcher.Close()
+	// 阻塞 main goroutine
+	<-make(chan struct{})
 }
 
 func startWatch(watcher *fsnotify.Watcher) {
 	var schedulerEvent *scheduler.Event
-	for {
-		select {
-		case event, ok := <-watcher.Events:
-			log.Printf("%v, %v", event, ok)
-			if !ok {
-				return
-			}
-			schedulerEvent = &scheduler.Event{
-				Name: event.Op.String(),
-				Path: event.Name,
-			}
-			signal := schedulerEvent.IgnoreSignal()
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				log.Printf("%v, %v", event, ok)
+				if !ok {
+					return
+				}
+				schedulerEvent = &scheduler.Event{
+					Name: event.Op.String(),
+					Path: event.Name,
+				}
+				signal := schedulerEvent.IgnoreSignal()
 
-			if signal {
-				schedulerEvent.ModelSchedulerModel(watcher)
-			}
+				if signal {
+					schedulerEvent.ModelSchedulerModel(watcher)
+				}
 
-			log.Println(event.String())
-		case err, ok := <-watcher.Errors:
-			log.Printf("err: %v, %v", err, ok)
-			if !ok {
-				log.Println("error:", err)
-				return
+				log.Println(event.String())
+			case err, ok := <-watcher.Errors:
+				log.Printf("err: %v, %v", err, ok)
+				if !ok {
+					log.Println("error:", err)
+					return
+				}
 			}
 		}
-	}
+	}()
 }
 
 func watcherDirs(dirPth string, watcher *fsnotify.Watcher) {
